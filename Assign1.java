dee3112f0b7592aa66f393efa87f24139452c218
yashashree07 <suryawanshiyashashree@gmail.com>
2020-12-14 18:05:55 +0530

    modified file assign1

--- Assign1.java
+++ Assign1.java
@@ -3,6 +3,8 @@ class Assignment1
 	public static void main(String args[])
 	{
 		System.out.println("ASSIGNMENT1 DONE");
+		System.out.println("Hello World");
+		System.out.println("Jenkins-git Demo");
 
 	}
 
