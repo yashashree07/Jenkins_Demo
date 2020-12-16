e565fba9b5f3677c2df1c9ca7d98824039764fae
yashashree07 <47655034+yashashree07@users.noreply.github.com>
2020-12-16 11:14:34 +0530

    Update Assign1.java

--- Assign1.java
+++ Assign1.java
@@ -2,7 +2,7 @@ class Assignment1
 {
 	public static void main(String args[])
 	{
-		int n1=1;n2=2;
+		int n1=1,n2=2;
 		int add=n1+n2;
 		System.out.println("ASSIGNMENT1 DONE");
 		System.out.println("Hello World");
