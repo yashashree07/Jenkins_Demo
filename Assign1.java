c8388d731eb1fdb89fd53cb8458021c8dc765cd2
yashashree07 <47655034+yashashree07@users.noreply.github.com>
2020-12-16 11:12:28 +0530

    Update Assign1.java

--- Assign1.java
+++ Assign1.java
@@ -2,9 +2,12 @@ class Assignment1
 {
 	public static void main(String args[])
 	{
+		int n1=1;n2=2;
+		int add=n1+n2;
 		System.out.println("ASSIGNMENT1 DONE");
 		System.out.println("Hello World");
 		System.out.println("Jenkins-git Demo");
+		System.out.println("Result :"+add);
 
 	}
 
